838fdc4a714b3d5e5e3a4eb4239d48d30b617feb
siaccarino <silvio@iaccarino.de>
2025-09-21 14:52:29 +0000

    second test, fix duplicated pytests in test explorer

--- tests/test_modbus_connect.py
+++ tests/test_modbus_connect.py
@@ -1,6 +1,7 @@
 from os import environ
 import pytest
 from custom_components.solakon_one import SolakonModbusHub
+from custom_components.solakon_one.const import DEFAULT_PORT, DEFAULT_SLAVE_ID
 from unittest.mock import MagicMock
 import asyncio
 
@@ -14,11 +15,26 @@ def test_modbus_connection(hass):
     client = SolakonModbusHub(
         hass,
         environ["SOLAKON_ONE_HOST"],
-        int(environ["SOLAKON_ONE_PORT"]),
-        int(environ["SOLAKON_ONE_MODBUS_SLAVE_ID"]),
+        int(environ.get("SOLAKON_ONE_PORT", DEFAULT_PORT)),
+        int(environ.get("SOLAKON_ONE_MODBUS_SLAVE_ID", DEFAULT_SLAVE_ID)),
         10,
     )
 
     result = asyncio.run(client.async_test_connection())
 
     assert result
+
+def test_get_device_info(hass):
+    client = SolakonModbusHub(
+        hass,
+        environ["SOLAKON_ONE_HOST"],
+        int(environ.get("SOLAKON_ONE_PORT", DEFAULT_PORT)),
+        int(environ.get("SOLAKON_ONE_MODBUS_SLAVE_ID", DEFAULT_SLAVE_ID)),
+        10,
+    )
+
+    result = asyncio.run(client.async_get_device_info())
+
+    assert result is not None
+    assert "manufacturer" in result
+
